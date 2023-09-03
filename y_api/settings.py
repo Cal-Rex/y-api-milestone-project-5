047ed8ee02f57b1b2d7dec36fea93d15e9dad7d7
Cal-Rex <andy.gibb@healthcareskills.com>
2023-09-03 20:27:46 +0000

    update allowed hosts ahead of deployment

--- y_api/settings.py
+++ y_api/settings.py
@@ -33,7 +33,10 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['8000-calrex-yapimilestonepro-z23jn325lep.ws-eu104.gitpod.io']
+ALLOWED_HOSTS = [
+    '8000-calrex-yapimilestonepro-z23jn325lep.ws-eu104.gitpod.io', 
+    os.environ.get('ALLOWED_HOST')
+]
 
 
 # Application definition
