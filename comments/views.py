95acdda68c01e00ea47629d0350f6f1ee697c455
Cal-Rex <andy.gibb@healthcareskills.com>
2023-09-03 08:24:39 +0000

    add comment detail view

--- comments/views.py
+++ comments/views.py
@@ -6,6 +6,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Comment
 from .serializers import CommentSerializer
+from y_api.permissions import IsOwnerOrReadOnly
 
 
 class CommentList(generics.ListCreateAPIView):
@@ -16,4 +17,11 @@ class CommentList(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
-        
+
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = CommentSerializer
+    queryset = Comment.objects.annotate(
+        votes_count=Count('parent_comment', distinct=True)
+    )
