8bbe52196b558317545d3aba12155e745639519a
Cal-Rex <andy.gibb@healthcareskills.com>
2023-09-03 15:31:40 +0000

    add filters and permissions to likes view

--- likes/views.py
+++ likes/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, filters, permissions
 from rest_framework.views import APIView
 from .models import Like
 from .serializers import LikeSerializer
 from y_api.permissions import IsOwnerOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class LikeList(generics.ListCreateAPIView):
@@ -13,8 +14,26 @@ class LikeList(generics.ListCreateAPIView):
     see what post was liked and who owns
     the likes
     """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = LikeSerializer
     queryset = Like.objects.all()
+    filter_backends = [
+        filters.OrderingFilter,
+        DjangoFilterBackend
+    ]
+    ordering_fields = [
+        'owner',
+        'post',
+        'post__likes_count',
+        'post__comments_count'
+    ]
+    filterset_fields = [
+        # filter by:
+        # likes by user
+        'owner__profile',
+        # likes by post
+        'post'
+    ]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
