5abe9908b9e74d4fce4fff427073549a6cb75d51
Cal-Rex <andy.gibb@healthcareskills.com>
2023-09-03 15:54:40 +0000

    add filters and permissions to votes views

--- votes/views.py
+++ votes/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import generics, filters
+from rest_framework import generics, filters, permissions
 from rest_framework.views import APIView
 from .models import Vote
 from .serializers import VoteSerializer
@@ -13,8 +13,28 @@ class VoteList(generics.ListCreateAPIView):
     made on comments.
     see who votes on what comment from what post
     """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = VoteSerializer
     queryset = Vote.objects.all()
+    filter_backends = [
+        filters.OrderingFilter,
+        DjangoFilterBackend
+    ]
+    ordering_fields = [
+        'owner',
+        'comment',
+        'comment__votes_count'
+    ]
+    filterset_fields = [
+        # filter by
+        # votes by user
+        'owner__profile',
+        # votes on post
+        'comment__post',
+        # votes on comment
+        'comment',
+    ]
+
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
