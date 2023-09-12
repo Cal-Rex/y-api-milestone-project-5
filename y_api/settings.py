fbeec16cf433a4b9c9044441a92f435f22768890
Cal-Rex <andy.gibb@healthcareskills.com>
2023-09-12 11:46:29 +0000

    remove obsolete print statement

--- y_api/settings.py
+++ y_api/settings.py
@@ -165,7 +165,6 @@ else:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
-    print("WOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO connected to external database")
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
